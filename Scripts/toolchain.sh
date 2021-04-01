8d6d396f57021834b3d6238a882a6c2e96e0d4ad
Changbeom Ahn <kewlbear@gmail.com>
2021-04-01 14:53:46 +0900

    add CI

---
+++ Scripts/toolchain.sh
@@ -0,0 +1,16 @@
+git clone --depth 1 https://github.com/kewlbear/kivy-ios.git
+cd kivy-ios
+
+python3 -m venv venv
+. venv/bin/activate
+
+pip install -e .
+pip install cython
+
+python toolchain.py build numpy
+
+mv dist/frameworks/libn* .
+mv dist/frameworks/numpy.xcframework libnumpy.xcframework
+
+rm -rf ../Sources/NumPySupport/site-packages
+mv dist/root/python3/lib/python3.8/site-packages ../Sources/NumPySupport
