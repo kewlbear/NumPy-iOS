cc15ad001cf1b9423888dc42480f9ea220af5b47
Changbeom Ahn <kewlbear@gmail.com>
2021-04-12 13:45:44 +0900

    fix ERROR ITMS-90171: "Invalid Bundle Structure ..."

--- Scripts/toolchain.sh
+++ Scripts/toolchain.sh
@@ -14,3 +14,5 @@ mv dist/frameworks/numpy.xcframework libnumpy.xcframework
 
 rm -rf ../Sources/NumPySupport/site-packages
 mv dist/root/python3/lib/python3.8/site-packages ../Sources/NumPySupport
+
+find ../Sources/NumPySupport/site-packages -name lib\*.a -delete
