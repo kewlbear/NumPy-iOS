1e3e624224fae44b8bbb576113e5b36fc5ad8f30
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-07-27 00:10:50 +0900

    fix linker settings

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "NumPy-iOS",
-            targets: ["libnumpy", "libnpymath", "libnpyrandom", "Link", "NumPySupport"]),
+            targets: ["libnumpy", "libnpymath", "libnpyrandom", "NumPySupport"]),
     ],
     dependencies: [
         .package(url: "https://github.com/kewlbear/Python-iOS.git", .branch("3.8_kivy-ios")),
@@ -17,13 +17,15 @@ let package = Package(
         .binaryTarget(name: "libnpymath", url: "https://github.com/kewlbear/NumPy-iOS/releases/download/0.0.20210504052912/libnpymath.xcframework.zip", checksum: "a2385b000e873692b4a25f891be0896518bace00bc855f4d41a1e6a4f7011eeb"),
         .binaryTarget(name: "libnpyrandom", url: "https://github.com/kewlbear/NumPy-iOS/releases/download/0.0.20210504052912/libnpyrandom.xcframework.zip", checksum: "2e34f4f9105c1e488597c4c5e808b5adefc3d187fa07b6775bb4f0714481fe6e"),
         .target(
-            name: "Link",
-            dependencies: ["libnumpy", "libnpymath", "libnpyrandom", "Python-iOS"],
-            linkerSettings: [.linkedFramework("Accelerate")]),
-        .target(
             name: "NumPySupport",
-            dependencies: ["Python-iOS"],
-            resources: [.copy("site-packages")]),
+            dependencies: ["libnumpy", "libnpymath", "libnpyrandom", "Python-iOS"],
+            resources: [.copy("site-packages")],
+            linkerSettings: [
+                .linkedFramework("Accelerate"),
+                .unsafeFlags([
+                    "-all_load"
+                ])
+            ]),
         .testTarget(
             name: "NumPy-iOSTests",
             dependencies: ["NumPySupport"]),
